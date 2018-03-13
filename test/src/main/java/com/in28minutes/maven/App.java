a40451deac7d22ab2c07c480304cd1fd34a86cfd
joelmacwan <37294758+joelmacwan@users.noreply.github.com>
2018-03-13 16:11:12 +0530

    commit changes

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World Test!" );
+	     System.out.println( "Hello World Test!" );
     }
 
 	public int calculateSomething() {
